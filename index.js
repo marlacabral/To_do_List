8038c6e7ddbd512d84755bd98b4e85dc434b1854
marlacabral <roncolicabral@gmail.com>
2021-09-10 15:11:24 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const Conn = require('./models/conn/conn');
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+console.log(db_url, db_user, db_pass, db_data);
+Conn(db_url, db_user, db_pass, db_data);
+
+const port = 3000;
+
+const tarefasRouter = require('./routers/tarefas.routes');
+app.use('/tarefas', tarefasRouter);
+
+app.listen(process.env.PORT || port, ()=> {
+    console.info(`Servidor rodando na porta ${port}`);
+  })
+
---
+++ routers/tarefas.routes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const Tarefa  = require('../models/tarefas');
+
+router.post("/add", async (req, res) => {
+    await Tarefa.create(req.body)
+    .then(() => {
+        res.status(200).send("Prontinho! Tarefa adicionada com sucesso. Tenha um ótimo dia.");
+
+    }).catch((err) => {
+        res.status(400).send("Opa! Não conseguimos incluir a sua nova tarefa, será que você preencheu tudo certinho?");
+        console.error(err);
+
+    })
+});
+
+router.get('/', async (req, res) => {
+    await Tarefa.find({})
+    .then((tarefa) => {
+        console.log(tarefa);
+        (res.status(200).send(tarefa));
+    })
+    .catch((err) => {
+        res.status(400).send("Opa! Não conseguimos fazer a sua busca, será que você preencheu tudo certinho?");
+        console.error(err);
+    })
+});
+
+router.get('/findById/:id', async (req, res) => {
+    await Tarefa.find({_id : req.params.id})
+    .then((tarefa) => {
+        res.status(200).send(tarefa);
+    })
+    .catch((err) => {
+        res.status(400).send("Opa! Não conseguimos fazer a sua busca, será que você preencheu tudo certinho?");
+        console.error(err);
+    })
+
+});
+
+router.put('/update:id', async (req, res) => {
+    await Tarefa.updateOne({_id : req.params.id},
+        req.body)
+        .then(() => {
+            res.status(400).send("Prontinho! Tarefa alterada com sucesso. Tenha um ótimo dia.");
+        })
+        .catch((err) => {
+            res.status(200).send("Opa! Não conseguimos fazer a sua inclusão, será que você preencheu tudo certinho?");
+            console.error(err);
+        });
+});
+
+router.delete("/delete", async (req, res) => {
+    await Tarefa.deleteOne({_id : req.params.id})
+    .then(() => {
+        res.status(200).send("Prontinho! Tarefa deletada com sucesso. Tenha um ótimo dia.");
+    })
+    .catch((err) => {
+        res.status(400).send("Opa! Não conseguimos fazer a sua inclusão, será que você preencheu tudo certinho?");
+        console.error(err);
+    })
+});
+
+module.exports = router;
+
