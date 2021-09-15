994084114d69a0d0b89a401c0453a67aa609e539
marlacabral <roncolicabral@gmail.com>
2021-09-15 19:00:03 -0300

    final page add

--- models/tarefas.js
+++ models/tarefas.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const tarefaModel = new mongoose.Schema({
+    
     titulo: { type: String, required: true },
     descricao: { type: String, required: true },
     prioridade: { type: String, required: true },
